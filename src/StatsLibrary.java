3c07e0a74090563156ecc3bfc2631a37bca6682c
violetann894 <rachelhuss2976@gmail.com>
2025-01-27 14:04:17 -0500

    Completed methods

--- src/StatsLibrary.java
+++ src/StatsLibrary.java
@@ -26,12 +26,17 @@ public class StatsLibrary {
 
     public static double median(double[] values){
         if(values.length % 2 == 1){
+
             int index = (int)Math.ceil(values.length / 2);
+
             return values[index];
         }else {
+
             int index = values.length / 2;
+
             double firstValue = values[index];
             double secondValue = values[index + 1];
+
             return (firstValue + secondValue) / 2.0;
         }
     }
@@ -50,14 +55,76 @@ public class StatsLibrary {
 
     }
 
-    public static double mode(double[] values){
+    public static ArrayList<Double> mode(double[] values){
+        ArrayList<Double> modes = new ArrayList<>();
+        double previousNum = 0.0;
+        int highestNumOfOccur = 1;
+        int currentNumOccur = 0;
 
-        return 0.0;
+        for(double value : values) {
+            if (value > previousNum) {
+                if (currentNumOccur == highestNumOfOccur) {
+                    modes.add(previousNum);
+                    currentNumOccur = 0;
+                } else if (currentNumOccur > highestNumOfOccur) {
+                    modes.clear();
+                    modes.add(previousNum);
+                    highestNumOfOccur = currentNumOccur;
+                    currentNumOccur = 0;
+                } else {
+                    currentNumOccur = 0;
+                }
+            }
+            currentNumOccur++;
+            previousNum = value;
+        }
+
+        if (currentNumOccur == highestNumOfOccur) {
+            modes.add(previousNum);
+            currentNumOccur = 0;
+        } else if (currentNumOccur > highestNumOfOccur) {
+            modes.clear();
+            modes.add(previousNum);
+            highestNumOfOccur = currentNumOccur;
+        }
+
+        return modes;
     }
 
-    public static double mode (ArrayList<Double> values){
+    public static ArrayList<Double> mode (ArrayList<Double> values){
+        ArrayList<Double> modes = new ArrayList<>();
+        double previousNum = 0.0;
+        int highestNumOfOccur = 1;
+        int currentNumOccur = 0;
 
-        return 0.0;
+        for(double value : values) {
+            if (value > previousNum) {
+                if (currentNumOccur == highestNumOfOccur) {
+                    modes.add(previousNum);
+                    currentNumOccur = 0;
+                } else if (currentNumOccur > highestNumOfOccur) {
+                    modes.clear();
+                    modes.add(previousNum);
+                    highestNumOfOccur = currentNumOccur;
+                    currentNumOccur = 0;
+                } else {
+                    currentNumOccur = 0;
+                }
+            }
+            currentNumOccur++;
+            previousNum = value;
+        }
+
+        if (currentNumOccur == highestNumOfOccur) {
+            modes.add(previousNum);
+            currentNumOccur = 0;
+        } else if (currentNumOccur > highestNumOfOccur) {
+            modes.clear();
+            modes.add(previousNum);
+            highestNumOfOccur = currentNumOccur;
+        }
+
+        return modes;
     }
 
     public static double standardDeviation(double[] values){
