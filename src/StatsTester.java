67e95894b7c1f3420b9f5bde8a376c4ba7ab3914
violetann894 <rachelhuss2976@gmail.com>
2025-01-27 14:06:17 -0500

    Removed testing statements

--- src/StatsTester.java
+++ src/StatsTester.java
@@ -3,18 +3,7 @@ import java.util.ArrayList;
 public class StatsTester {
 
     public static void main(String[] args) {
-        ArrayList<Double> values = new ArrayList<>();
-        values.add(1.0);
-        values.add(5.6);
-        values.add(6.9);
-        values.add(6.9);
-        values.add(7.2);
-        values.add(8.0);
-        values.add(9.5);
 
-        System.out.println(StatsLibrary.mean(values));
-        System.out.println(StatsLibrary.standardDeviation(values));
-        System.out.println(StatsLibrary.median(values));
 
     }
 
