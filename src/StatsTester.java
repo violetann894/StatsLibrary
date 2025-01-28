13ea41adc4a5d40913e26cadd3eb205f159adf9a
violetann894 <rachelhuss2976@gmail.com>
2025-01-27 23:22:46 -0500

    Added all tester cases

--- src/StatsTester.java
+++ src/StatsTester.java
@@ -13,6 +13,12 @@ public class StatsTester {
         ArrayList<Integer> intArrayListOdd = new ArrayList<>();
         ArrayList<Integer> intArrayListEven = new ArrayList<>();
 
+        double[] doubleValuesMode = {1.1, 1.1, 3.5, 3.5, 6.2, 7.9, 10.3};
+        ArrayList<Double> doubleArrayListMode = new ArrayList<>();
+
+        int[] intValuesMode = {1, 1, 2, 3, 3, 6, 7, 8, 9, 9};
+        ArrayList<Integer> intArrayListMode = new ArrayList<>();
+
         doubleArrayListOdd.add(1.1);
         doubleArrayListOdd.add(2.5);
         doubleArrayListOdd.add(3.7);
@@ -35,12 +41,34 @@ public class StatsTester {
         intArrayListEven.add(3);
         intArrayListEven.add(4);
 
+        doubleArrayListMode.add(1.1);
+        doubleArrayListMode.add(1.1);
+        doubleArrayListMode.add(3.5);
+        doubleArrayListMode.add(3.5);
+        doubleArrayListMode.add(6.2);
+        doubleArrayListMode.add(7.9);
+        doubleArrayListMode.add(10.3);
+
+        intArrayListMode.add(1);
+        intArrayListMode.add(1);
+        intArrayListMode.add(2);
+        intArrayListMode.add(3);
+        intArrayListMode.add(3);
+        intArrayListMode.add(6);
+        intArrayListMode.add(7);
+        intArrayListMode.add(8);
+        intArrayListMode.add(9);
+        intArrayListMode.add(9);
+
+
         StatsLibrary tester = new StatsLibrary();
 
         System.out.println("Result of mean (using double array): " + tester.getMeanDouble(doubleValuesOdd));
-        System.out.println("Result of mean (using ArrayList<Double>: " + tester.getMeanDouble(doubleArrayListOdd));
+        System.out.println("Result of mean (using ArrayList<Double>): " + tester.getMeanDouble(doubleArrayListOdd));
         System.out.println("Result of mean (using int array): " + tester.getMeanInt(intValuesOdd));
-        System.out.println("Result of mean (using ArrayList<Integer>: " + tester.getMeanInt(intArrayListOdd));
+        System.out.println("Result of mean (using ArrayList<Integer>): " + tester.getMeanInt(intArrayListOdd));
+
+        System.out.println();
 
         System.out.println("Result of median (using odd double array): " + tester.getMedianDouble(doubleValuesOdd));
         System.out.println("Result of median (using even double array): " + tester.getMedianDouble(doubleValuesEven));
@@ -53,12 +81,14 @@ public class StatsTester {
         System.out.println("Result of median (using odd ArrayList<Integer>): " + tester.getMeanInt(intArrayListOdd));
         System.out.println("Result of median (using even ArrayList<Integer>): " + tester.getMeanInt(intArrayListEven));
 
-        /*
-        System.out.println("Result of mode (using double array): " +  );
-        System.out.println("Result of mode (using ArrayList<Double>): " + );
-        System.out.println("Result of mode (using int array): " + );
-        System.out.println("Result of mode (using ArrayList<Integer>): " + );
-        */
+        System.out.println();
+
+        System.out.println("Result of mode (using double array): " + tester.getModeDouble(doubleValuesMode));
+        System.out.println("Result of mode (using ArrayList<Double>): " + tester.getModeDouble(doubleArrayListMode));
+        System.out.println("Result of mode (using int array): " + tester.getModeInt(intValuesMode));
+        System.out.println("Result of mode (using ArrayList<Integer>): " + tester.getModeInt(intArrayListMode));
+
+        System.out.println();
 
         System.out.println("Result of standard deviation (using double array): " +
                 tester.getStandardDeviationDouble(doubleValuesOdd));
